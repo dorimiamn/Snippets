3ad137ca3532694e6de1ed3a062291cbef2b1834
dorimiamn <36445436+dorimiamn@users.noreply.github.com>
2023-06-25 23:33:06 +0900

    Update template.cpp

--- library/template.cpp
+++ library/template.cpp
@@ -6,8 +6,8 @@ typedef long long ll;
 typedef long double ld;
 typedef pair<ll, ll> pll;
 
-const ll LongINF=1e13+7;
-const int INF=1e9+7;
+const ll LongINF=10000000000000;
+const int INF=1000000007;
 const int dx[]={0,1,0,-1};
 const int dy[]={1,0,-1,0};
 
@@ -15,8 +15,7 @@ template<class T> inline bool chmin(T& a, T b) {if (a > b) {a = b;return true;}r
 template<class T> inline bool chmax(T& a, T b) {if (a < b) {a = b;return true;}return false;}
 template <class T, class Y>T GCD(T a, Y b){if(a<b){int c=a;a=b;b=c;}if (a % b == 0){return b;}return GCD(b, a % b);}
 template<class T,class Y>T LCM(T a,Y b){return (a*b)/GCD(a,b);}
-template<class T>T modpow(T a,T n,T mod){ll res=1;while(n>0){if(n&1)res=res*a%mod;a=a*a%mod;n>>=1;}return res;}
-void clear(queue<pair<int, int>> &q){queue<pair<int, int>> empty;swap(q, empty);} //Don't forget changing Type inside Queue
+void clear(queue<pair<int, int>> &q){queue<pair<int, int>> empty;swap(q, empty);} //queueの中身の型は適時変更を忘れない
 
 using vi=vector<int>;
 using vii=vector<vi>;
@@ -32,6 +31,7 @@ int main(){
 
     return 0;
 }
+
 /*
 
 */
